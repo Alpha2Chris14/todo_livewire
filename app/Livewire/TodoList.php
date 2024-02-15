6f9b604179f26925f127a533c1bdde8af0d6fe8d
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-02-15 18:23:43 +0100

    added create,store,delete

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\todo;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class TodoList extends Component
+{
+    use WithPagination;
+    #[Rule("required|min:4|max:100")]
+    public $name;
+
+    public $search;
+
+    public function create(){
+        // validate the input
+        $validated = $this->validateOnly('name');
+        // create the todo
+        todo::create($validated);
+        // clear the input
+        $this->reset();
+        // send flash message
+        session()->flash("success","Todo created successfully");
+    }
+
+    public function render()
+    {
+        $data['todos'] = Todo::latest()->where("name","like","%{$this->search}%")->paginate(5);
+        return view('livewire.todo-list',$data);
+    }
+
+    public function delete($id){
+        Todo::find($id)->delete();
+    }
+}
---
+++ resources/views/livewire/todo-list.blade.php
@@ -0,0 +1,13 @@
+<div>
+    @include('livewire.include.create-todo-box')
+    @include('livewire.include.search-box')
+    <div id="todos-list">
+        @foreach ($todos as $todo)
+            @include('livewire.include.todo-card')
+        @endforeach
+        <div class="my-2">
+            <!-- Pagination goes here -->
+            {{$todos->links()}}
+        </div>
+    </div>
+</div>
